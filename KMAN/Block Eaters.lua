435e26c8fe402c6fb86f731e561038fa731d62c1
DOZE / UILib <afterlife.records@outlook.com>
2026-03-28 22:50:43 +0000

    Create Block Eaters.lua

---
+++ KMAN/Block Eaters.lua
@@ -0,0 +1,5 @@
+local args = {
+	"BlobSize",
+	2002313123123
+}
+game:GetService("ReplicatedStorage"):WaitForChild("Honeypot"):WaitForChild("Internal"):WaitForChild("RemoteStorage"):WaitForChild("ClaimBoost - RemoteEvent"):FireServer(unpack(args))
